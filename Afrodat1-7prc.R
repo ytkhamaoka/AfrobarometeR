e65f8d78ce9931f83b6f794c9f56e4a1ddcc2f43
ytkhamaoka <58832066+ytkhamaoka@users.noreply.github.com>
2019-12-16 17:28:32 +0900

    update

--- Afrodat1-7prc.R
+++ Afrodat1-7prc.R
@@ -82,8 +82,51 @@ min2<-function(x){
 	return(m)
 	}
 
+#
+table2<-function(x1,x2,...){ 
+	st<-table(x1,x2,...) 
+	Ic<-matrix(1,nrow(st),1) 
+	Ir<-matrix(1,ncol(st),1) 
+		sr<-st%*%Ir					#行和 
+		sc<-t(st)%*%Ic			#列和 
+		s0<-t(sr)%*%Ic			#総和 
+	#----------------------- 
+	ss0<-st 
+ 	sst<-rbind(st,t(sc)) 		 #行和、列和を入れた行列に 
+	sc<-rbind(sc,s0) 		#列和にさらに総計を入れておく 
+	sr<-rbind(sr,s0) 		#行和にさらに総計を入れておく
+	 sst<-cbind(sst,sr)   #もとの表に列和、行和、総和がついたもの
+	 print(sst)
+ #-------------------------------- 行和を100% 
+ pr<-sst 
+	 for (j in seq(1:ncol(sst))){ 
+ 	pr[,j]<-pr[,j]/sr*100 
+ 	} 
+ #小数点1桁までで丸めて出力 
+ pr<-round( pr,1) 
+	 pr<-cbind(pr,sr)		#和のベクトルをつけて返す
+			colnames(pr)[length(colnames(pr))-1]<-"%"
+			colnames(pr)[length(colnames(pr))]<-"N"
+ #列和を100% 
+ pc<-sst 
+ for (i in seq(1:nrow(sst))){ 
+	 pc[i,]<-pc[i,]/t(sc)*100 
+	 } 
+ pc<-round( pc,1) 
+ #総和を100% 
+ p0<-sst 
+ for (i in seq(1:nrow(sst))){ 
+ 	for (j in seq(1:ncol(sst))){ 
+		 p0[i,j]<-p0[i,j]/s0*100 
+	 } 
+	 } 
+ p0<-round( p0,1) 
+	chisq<-chisq.test(st)
+	return(list(pr,chisq))
+ } 
+ 
 #-------------------------------
-dwaw.line2<-function(dat){		#datの中にあるtを横軸､yを縦軸にとってプロット｡左端に国記号　dat$t:year, dat$y, dat$cnam: country name
+dwaw.line2<-function(dat){		#datの中にあるtを横軸､yを縦軸にとってプロット｡左端に国記号　変数名は固定　dat$t:year, dat$y, dat$cnam: country name
 	d<-dim(dat)[1]
 	lines(dat$t,dat$y)
 		text(dat$t[d],dat$y[d],dat$cnam[d],pos=4,cex=0.5)		#最後のtのところにcountry nameを
@@ -124,21 +167,47 @@ as.data.frame(m)
 
 
 #単純にfactorだと変数が多い
-res<-glm(Own_TV~Age+Gender_f+Education+as.factor(Employment_status)+as.factor(Race)+as.factor(Occupation)+Mem_religious+Mem_voluntary+gone_food+gone_cash+as.factor(Language)+COUNTRY2,family="binomial",data=Afrodat6)
+#res<-glm(Own_TV~Age+Gender_f+Education+as.factor(Employment_status)+as.factor(Race)+as.factor(Occupation)+Mem_religious+Mem_voluntary+gone_food+gone_cash+as.factor(Language)+COUNTRY2,family="binomial",data=Afrodat6)
 	summary(res)
 
+table2(Afrodat6$Mem_religious,Afrodat6$Mem_voluntary,exclude=NULL)
+#        0    1    2    3 <NA>   %     N
+#0    78.6  8.8  9.4  3.0  0.2 100 28141
+#1    49.8 34.1 12.4  3.2  0.5 100  8986
+#2    46.2 11.7 35.9  5.6  0.6 100 13739
+#3    37.9  8.5 26.7 26.4  0.5 100  2718
+#<NA> 11.7  3.4  5.7  1.7 77.5 100   351
+#     63.1 13.7 17.5  4.9  0.9 100 53935
+
+
+table2(Afrodat6$Own_Radio,Afrodat6$News_Radio,exclude=NULL)
+#radioなしでもニュースソースにしている者も
+#        0   1   2    3    4 <NA>   %     N
+#0    45.5 5.9 9.8 19.7 18.8  0.2 100 15825
+#1     7.0 2.5 5.9 22.9 61.5  0.1 100 38038
+#<NA> 13.9 4.2 8.3 16.7 44.4 12.5 100    72
+#     18.3 3.5 7.1 21.9 49.0  0.2 100 53935
+table2(Afrodat6$Own_TV,Afrodat6$News_Television,exclude=NULL)
+#        0   1   2    3    4 <NA>   %     N
+#0    70.9 5.3 5.7  8.3  9.5  0.2 100 27034
+#1     7.0 2.3 5.1 18.2 67.3  0.2 100 26812
+#<NA> 37.1 5.6 3.4 10.1 29.2 14.6 100    89
+#     39.1 3.8 5.4 13.2 38.3  0.2 100 53935
+
+cor(Afrodat6[,c("Own_Auto","Own_Mbphone","Own_Radio","Own_TV","News_Radio","News_Television","News_Internet","News_Social_media")],use="complete")
+	 
 #ダミー変数で
-v<-as.formula('~Age+Gender_f+Education+
+v<-as.formula('~gone_food+gone_cash+
+Mem_religious+Mem_voluntary+
+Age+Gender_f+Education+
 dEmployment_status_looking+dEmployment_status_part_time+dEmployment_status_full_time+
 dOccupation_Never+dOccupation_Student+dOccupation_Housewife_homemaker+dOccupation_primary+dOccupation_Trader+dOccupation_Retail+dOccupation_Unskilled+dOccupation_skilled+dOccupation_Clerical+dOccupation_Supervisor+dOccupation_police+dOccupation_Mid_level+dOccupation_Upper_level+	
-Mem_religious+Mem_voluntary+
-gone_food+gone_cash+
-dlang_English+dlang_French+dlang_Portuguese+dlang_Swahili+dlang_Arabic+dlang_Afrikaans+dlang_Chichewa+dlang_Akan+dlang_Other+dlang_Egyptian_Arabic+dlang_Crioulo+dlang_Kirund+dlang_Sesotho+dlang_Sudanese_Arabic+dlang_Creole+dlang_siSwati+dlang_Shona+dlang_Algerian_Arabic+
 dRace_BAf+dRace_Wh+dRace_Col+dRace_Arab+dRace_SAs+dRace_EAs	+
+dlang_English+dlang_French+dlang_Portuguese+dlang_Swahili+dlang_Arabic+dlang_Afrikaans+dlang_Chichewa+dlang_Akan+dlang_Other+dlang_Egyptian_Arabic+dlang_Crioulo+dlang_Kirund+dlang_Sesotho+dlang_Sudanese_Arabic+dlang_Creole+dlang_siSwati+dlang_Shona+dlang_Algerian_Arabic+
 dCOUNTRY_ALG+dCOUNTRY_BDI+dCOUNTRY_BFO+dCOUNTRY_CAM+dCOUNTRY_CDI+dCOUNTRY_EGY+dCOUNTRY_GAB+dCOUNTRY_GHA+dCOUNTRY_GUI+dCOUNTRY_KEN+dCOUNTRY_LES+dCOUNTRY_LIB+dCOUNTRY_MAD+dCOUNTRY_MAU+dCOUNTRY_MLI+dCOUNTRY_MLW+dCOUNTRY_MOR+dCOUNTRY_MOZ+dCOUNTRY_NAM+dCOUNTRY_NGR+dCOUNTRY_NIG+dCOUNTRY_SAF+dCOUNTRY_SEN+dCOUNTRY_SRL+dCOUNTRY_STP+dCOUNTRY_SUD+dCOUNTRY_SWZ+dCOUNTRY_TAN+dCOUNTRY_TOG+dCOUNTRY_TUN+dCOUNTRY_UGA+dCOUNTRY_ZAM')
 ##dOccupation_Other +dRace_Oth 
 
-cor(Afrodat6[,c("Own_Auto","Own_Mbphone","Own_Radio","Own_TV","News_Radio","News_Television","News_Internet","News_Social_media")],use="complete")
+
 
 res_oAuto<-glm(formula(paste("Own_Auto~",v,sep="")[2]),family="binomial",data=Afrodat6)
 	summary(res_oAuto)
@@ -163,6 +232,66 @@ res_nSocial_media<-lm(formula(paste("News_Social_media~",v,sep="")[2]),data=Afro
 mtable(res_oAuto,res_oMbphone,res_oRadio,res_nRadio,res_oTelevision,res_nTelevision,res_nInternet,res_nSocial_media)
 
 
+#保有状況でクラスタ
+set.seed(12345)
+d<-complete.cases(Afrodat6[,c("Own_Auto","Own_Mbphone","Own_Radio","Own_TV","News_Radio","News_Television","News_Internet","News_Social_media")])
+	sum(d);dim(Afrodat6)[1]	#52496  53935
+ocl3<-kmeans(Afrodat6[d,c("Own_Auto","Own_Mbphone","Own_Radio","Own_TV")],3)
+	ocl3$size
+	ocl3$centers
+
+ocl4<-kmeans(Afrodat6[d,c("Own_Auto","Own_Mbphone","Own_Radio","Own_TV")],4)
+	ocl4$size	# 18870  9615  1639 22372
+	ocl4$centers
+data.frame(ocl4$centers,ocl4$size)
+#    Own_Auto Own_Mbphone Own_Radio    Own_TV ocl4.size
+#1 0.56550079           1 0.5002650 0.6043455     18870	(4)全体的に高い
+#2 0.02953718           0 0.4284971 0.0000000      9615	(1)ラジオ
+#3 0.12751678           0 0.7156803 1.0000000      1639	(3)ラジオ､TV､自動車
+#4 0.00000000           1 1.0000000 0.5910066     22372	(2)携帯､ラジオ､TV
+
+ocl5<-kmeans(Afrodat6[d,c("Own_Auto","Own_Mbphone","Own_Radio","Own_TV")],5)
+	data.frame(ocl5$centers,ocl5$size)
+#    Own_Auto Own_Mbphone Own_Radio     Own_TV ocl5.size
+#1 0.02054329   0.0000000 0.0000000 0.06706282      5890	(1)全体的に低い
+#2 0.01228335   0.6895252 1.0000000 0.00000000     13270	(2)携帯とラジオ
+#3 0.00000000   1.0000000 0.6172448 0.74230895     21421	(3)携帯とラジオ､TV
+#4 0.16800643   0.0000000 0.9429260 1.00000000      1244	(4)クルマ､ラジオ､TV
+#5 1.00000000   1.0000000 0.8846406 0.81763659     10671	(5)全体的に高い
+
+#ニュースソース
+ucl3<-kmeans(Afrodat6[d,c("News_Radio","News_Television","News_Internet","News_Social_media")],3)
+	ucl3$size
+	ucl3$centers
+
+ucl4<-kmeans(Afrodat6[d,c("News_Radio","News_Television","News_Internet","News_Social_media")],4)
+	ucl4$size
+	ucl4$centers
+
+ucl5<-kmeans(Afrodat6[d,c("News_Radio","News_Television","News_Internet","News_Social_media")],5)
+data.frame(ucl5$centers,ucl5$size)
+#  News_Radio News_Television News_Internet News_Social_media ucl5.size
+#1  0.1755152      1.05501979     0.1099381        0.08750381      9851	(1)全体的に利用せず
+#2  3.5655794      3.51169832     0.1138866        0.05229838     14532	(3)ラジオ､TVのみ
+#3  3.4360568      3.49609236     2.5367673        2.28952043      5630	(4)利用するが　ネットは若干低い
+#4  2.9646811      3.58895625     3.8278861        3.85859251      7588	(5)全体的に利用
+#5  3.4563948      0.09405841     0.0537093        0.05176234     14895	(2)ラジオのみ
+
+dat<-data.frame(ocl5$cluster,ucl5$cluster)
+names(dat)<-c("own","usage")
+	dat$usage2<-ifelse(dat$usage==2,3,	#usageの番号うちなおし
+				ifelse(dat$usage==3,4,
+				ifelse(dat$usage==4,5,
+				ifelse(dat$usage==5,2,dat$usage))))
+				
+	table(dat$own,dat$usage2,exclude=NULL)
+	table2(dat$own,dat$usage2,exclude=NULL)
+
+
+#multivariate probit  #処理遅いので
+#resmP<-mvProbit(formula(paste("cbind(Own_Auto,Own_Mbphone,Own_Radio,Own_TV)~",v,sep="")[2]),data=Afrodat6[d,])
+#resmP<-mvProbit(cbind(Own_Auto,Own_Mbphone,Own_Radio,Own_TV)~gone_food+gone_cash+Mem_religious+Mem_voluntary+Age+Gender_f+Education+dEmployment_status_looking+dEmployment_status_part_time+dEmployment_status_full_time+dOccupation_Never+dOccupation_Student+dOccupation_Housewife_homemaker+dOccupation_primary+dOccupation_Trader+dOccupation_Retail+dOccupation_Unskilled+dOccupation_skilled+dOccupation_Clerical+dOccupation_Supervisor+dOccupation_police+dOccupation_Mid_level+dOccupation_Upper_level+	dRace_BAf+dRace_Wh+dRace_Col+dRace_Arab+dRace_SAs+dRace_EAs	+dlang_English+dlang_French+dlang_Portuguese+dlang_Swahili+dlang_Arabic+dlang_Afrikaans+dlang_Chichewa+dlang_Akan+dlang_Other+dlang_Crioulo+dlang_Kirund+dlang_Sesotho+dlang_Sudanese_Arabic+dlang_Creole+dlang_siSwati+dlang_Shona+dlang_Algerian_Arabic+dCOUNTRY_ALG+dCOUNTRY_BDI+dCOUNTRY_BFO+dCOUNTRY_CAM+dCOUNTRY_CDI+dCOUNTRY_EGY+dCOUNTRY_GAB+dCOUNTRY_GHA+dCOUNTRY_GUI+dCOUNTRY_KEN+dCOUNTRY_LES+dCOUNTRY_LIB+dCOUNTRY_MAD+dCOUNTRY_MAU+dCOUNTRY_MLI+dCOUNTRY_MLW+dCOUNTRY_MOR+dCOUNTRY_MOZ+dCOUNTRY_NAM+dCOUNTRY_NGR+dCOUNTRY_NIG+dCOUNTRY_SAF+dCOUNTRY_SEN+dCOUNTRY_SRL+dCOUNTRY_STP+dCOUNTRY_SUD+dCOUNTRY_SWZ+dCOUNTRY_TAN+dCOUNTRY_TOG+dCOUNTRY_TUN+dCOUNTRY_UGA+dCOUNTRY_ZAM,data=Afrodat6[d,])	#dlang_Egyptian_Arabic+	とdCOUNTRY_EGYが相関
+#resmP
 
 #---------------
 #			7
